a6ea84fcee85e25b356925fd3a0e782c46fc90cd
Jeff Brunson <jeff.a.brunson@gmail.com>
2015-10-28 14:20:27 -0600

    first commit

---
+++ index.ios.js
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var SearchPage = require('./SearchPage');
+
+var styles = React.StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
+  container: {
+    flex: 1
+  }
+});
+
+// class HelloWorld extends React.Component {
+//   render() {
+//     return <React.Text style={styles.text}>Hello World (Again)</React.Text>;
+//   }
+// }
+
+class PropertyFinderApp extends React.Component {
+  render() {
+    return (
+      <React.NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}/>
+    );
+  }
+}
+
+//defines entry point and provides root component
+React.AppRegistry.registerComponent('PropertyFinder', function() { return PropertyFinderApp });
